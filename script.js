0f25248dffe9ae9f36930117aa8228edd97aa3c7
ghaith060mcz <ghaith.mcz2000@gmail.com>
2024-10-17 13:47:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// إضافة تأثير عند تمرير الفأرة على الروابط
 document.querySelectorAll('.links a').forEach(link => {
     link.addEventListener('mouseover', () => {
-        link.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+        link.style.transform = 'scale(1.1)';
     });
+
     link.addEventListener('mouseout', () => {
-        link.style.boxShadow = 'none';
+        link.style.transform = 'scale(1)';
     });
 });
